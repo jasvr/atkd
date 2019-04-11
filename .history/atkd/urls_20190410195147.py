d50c96cb9a6baecd5234a48948a3d0a4a021f99d
Jasmin Vargas <jas.vrgs@gmail.com>
2019-04-10 21:49:22 -0400

    add base html and css

---
+++ .history/atkd/urls_20190410195147.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.parent_list, name='parent_list'),
+    path('parents/<int:pk>', views.parent_detail, name='parent_detail'),
+
+    path('students/', views.student_list, name='student_list')
+    path('songs/<int:pk>', views.student_detail.html, name='student_detail')
+    
+]
---
+++ .history/atkd/views_20190410194938.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Parent, Student
+
+def parent_list(request):
+    parents = Parent.objects.all()
+    return render(request, 'atkd/parent_list.html', {'parents': parents})
+
+def parent_detail(request, pk):
+    parent = Parent.objects.get(id=pk)
+    return render(request, 'atkd/parent_detail.html', {'parent': parent})    
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request,'atkd/student_list.html',{'students':students})
+
+def student_detail(request, pk):
+    student = Student.objects.get(id=pk)
+    return 
